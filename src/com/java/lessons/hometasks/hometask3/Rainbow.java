680ffb27135563e34d9b25bd129b3330405205f2
Dmitry Stepchenko <dmitry.enezi@gmail.com>
2022-10-22 22:54:23 +0300

    Initial commit

--- src/com/java/lessons/hometasks/hometask3/Rainbow.java
+++ src/com/java/lessons/hometasks/hometask3/Rainbow.java
@@ -1,41 +1,26 @@
 package com.java.lessons.hometasks.hometask3;
 
 public class Rainbow {
-    public static final String FIRST_COLOUR = "red";
-    public static final String SECOND_COLOUR = "orange";
-    public static final String THIRD_COLOUR = "yellow";
-    public static final String FOURTH_COLOUR = "green";
-    public static final String FIFTH_COLOUR = "light blue";
-    public static final String SIXTH_COLOUR = "blue";
-    public static final String SEVENTH_COLOUR = "purple";
+    public static final String RED = "red";
+    public static final String ORANGE = "orange";
+    public static final String YELLOW = "yellow";
+    public static final String GREEN = "green";
+    public static final String LIGHT_BLUE = "light blue";
+    public static final String BLUE = "blue";
+    public static final String PURPLE = "purple";
 
     public static String firstColour;
 
     public String getFirstColour (int numberOfColour) {
         switch (numberOfColour) {
-            case 1:
-                firstColour = FIRST_COLOUR;
-                break;
-            case 2:
-                firstColour = SECOND_COLOUR;
-                break;
-            case 3:
-                firstColour = THIRD_COLOUR;
-                break;
-            case 4:
-                firstColour = FOURTH_COLOUR;
-                break;
-            case 5:
-                firstColour = FIFTH_COLOUR;
-                break;
-            case 6:
-                firstColour = SIXTH_COLOUR;
-                break;
-            case 7:
-                firstColour = SEVENTH_COLOUR;
-                break;
-            default:
+            case 1: return RED;
+            case 2: return ORANGE;
+            case 3: return YELLOW;
+            case 4: return GREEN;
+            case 5: return LIGHT_BLUE;
+            case 6: return BLUE;
+            case 7: return PURPLE;
+            default: return "color is not found";
         }
-        return firstColour;
     }
 }
